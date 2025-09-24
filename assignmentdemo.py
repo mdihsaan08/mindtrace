c559e9644d54cf5e9b489ec97b9ef2fe4842f83e
Mohammed Ihsaan Mohamed <mm5801@g.rit.edu>
2025-09-24 13:51:22 +0400

    initial

---
+++ assignmentdemo.py
@@ -0,0 +1,26 @@
+##Check the input number from user is odd or even.
+#num = int(input("Enter your number: "))
+#if (num%2) == 0:
+#    print("The entered number is even.")
+#else: 
+#    print("The entered number is odd")
+
+#Check the input number from user is positive or negative.
+#num = int(input("Enter your number: "))
+#if num >= 0:
+#   print("The number entered is positive")
+#else:
+#   print("The number entered is negative")
+   
+
+##To find the square of given number.
+#num = int(input("Enter your number: "))
+#num = num * num 
+#print("The square of your number is", num)
+
+
+##To find the cube of given number.
+#num = int(input("Enter your number: "))
+#num = num * num * num
+#print("The cube of your number is", num)
+
